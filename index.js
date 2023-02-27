61ff5b798cc5b3351a101b23cdccc30c533ccac0
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-02-27 16:57:48 -0500

    assignemnt completed

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide = (a, b) => {
+    console.log(`dividing ${a}`);
+    console.log(`dividing ${b}`);
+    return a / b;
+  }
+  divide(2000,100)
+
+  const add = (a, b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+  }
+  add(3,4)
+
+  const square = (x) => {
+    console.log(`multiplying ${x}`);
+    console.log(`multiplying ${x}`);
+    return x * x;
+  }
+  square(2)
