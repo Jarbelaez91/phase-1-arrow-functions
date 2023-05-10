8236cee2f4138ac1b0d7942cf1106e7c7e3cb65c
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-05-10 16:48:48 -0500

    completed assignemnt

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const divide = (a, b) => {
-    console.log(`dividing ${a}`);
-    console.log(`dividing ${b}`);
-    return a / b;
-  }
-  divide(2000,100)
+const divide = function() {
+  let a = 2000;
+  let b = 100;
+  return a / b;
+}
 
-  const add = (a, b) => {
-    console.log(`Adding ${a}`);
-    console.log(`Adding ${b}`);
-    return a + b;
-  }
-  add(3,4)
+const square = x => x ** 2;
 
-  const square = (x) => {
-    console.log(`multiplying ${x}`);
-    console.log(`multiplying ${x}`);
-    return x * x;
-  }
-  square(2)
+let parameterA = 3;
+let parameterB = 4;
+let add= (parameterA, parameterB) => parameterA + parameterB;
